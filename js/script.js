559ff2d47fd2bdf0a4a2266c235a33125f4fa772
Débora Inocêncio <30512134+MaisQueJogos@users.noreply.github.com>
2021-10-18 16:22:08 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,10 @@
-
+var myWords = ["FOGÃO", "BALÃO", "PIÃO", "SABÃO", "CORAÇÃO", "ANÃO","LIMÃO","FEIJÃO","LEÃO","VIOLÃO"];
+$(document).ready(function(){
+    arrangeGame();                   
+});
+function arrangeGame()
+{
+  $.each(myWords, function(key, item){
+      $("#hint").append("<p>" + item + "</p>");
+  })
+}
